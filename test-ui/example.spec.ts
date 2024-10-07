10973e5476dab9ec0c73e8412bbab8f19920a2ab
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-10-07 09:58:43 +0200

    fix(example.spec): removed localhost

--- test-ui/example.spec.ts
+++ test-ui/example.spec.ts
@@ -2,11 +2,14 @@ import { expect } from "@playwright/test";
 import { test } from "./coverage_wrapper";
 
 test("find-watman", async ({ page }) => {  
-  await page.goto("http://localhost:8082/");
+  await page.goto("/");
   await expect(page.getByAltText("This is watman")).toBeInViewport();
 });
 
 test("find-fibonacci", async ({ page }) => {
-  await page.goto("http://localhost:8082/site_a.html");
+  await page.goto("/site_a.html");
   await expect(page.getByText("Factorial value ")).toBeVisible()
 });
+
+// YouTube Tests
+
