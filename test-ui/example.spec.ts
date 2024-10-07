9a29d83e4b02097916079617915dc951dff886b2
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-10-07 09:54:17 +0200

    feat(example.spec): added ui test

--- test-ui/example.spec.ts
+++ test-ui/example.spec.ts
@@ -2,6 +2,11 @@ import { expect } from "@playwright/test";
 import { test } from "./coverage_wrapper";
 
 test("find-watman", async ({ page }) => {  
-  await page.goto("/");
+  await page.goto("http://localhost:8082/");
   await expect(page.getByAltText("This is watman")).toBeInViewport();
+});
+
+test("find-fibonacci", async ({ page }) => {
+  await page.goto("http://localhost:8082/site_a.html");
+  await expect(page.getByText("Factorial value ")).toBeVisible()
 });
