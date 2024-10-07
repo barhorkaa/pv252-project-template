bb54fd88b1e0283214e5d3e673ffe0d2b86649d5
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-10-07 08:36:50 +0200

    feat(fibonacci.test): added unit test to increase coverage

--- src/fibonacci.test.ts
+++ src/fibonacci.test.ts
@@ -3,3 +3,18 @@ import { fibonacci } from "./fibonacci.ts";
 test("fibonacci-5", () => {
   expect(fibonacci(5)).toBe(5);
 });
+
+test("fibonacci-negative", () => {
+  const will_throw = () => {
+    fibonacci(-1);
+  };
+  expect(will_throw).toThrow("Cannot compute on negative numbers");
+});
+
+test("fibonacci-0", () => {
+  expect(fibonacci(0)).toBe(0);
+});
+
+test("fibonacci-1", () => {
+  expect(fibonacci(1)).toBe(1);
+});
